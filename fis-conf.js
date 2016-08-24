93bd5f8232a6b6a32dfafc353d4841372982680e
Your Name <you@example.com>
2016-08-24 15:23:37 +0800

    Revert "把注释去掉"
    This reverts commit a81de5cd8ca59ae717915f942451c98f9c3c6b7f.

--- fis-conf.js
+++ fis-conf.js
@@ -1,9 +1,19 @@
 fis.match('*.less', {
-  // fis-parser-less ????????????
+  // fis-parser-less 插件进行解析
   parser: fis.plugin('less'),
-  // .less ???????????????????? .css ????
+  // .less 文件后缀构建后被改成 .css 文件
   rExt: '.css'
 });
+//fis.match('::package', {
+//  postpackager: fis.plugin('loader', {
+//    allInOne: true
+//  })
+//});
+//
+//fis.match('*.less', {
+//  parser: fis.plugin('less'),
+//  rExt: '.css'
+//});
 fis.match('::packager', {
   spriter: fis.plugin('csssprites')
 });
